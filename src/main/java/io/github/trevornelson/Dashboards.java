70c5ed59041c41257da19050e9b4e4c0dc21e94c
trevornelson <trevor.nelson1@gmail.com>
2015-04-25 16:06:59 -0400

    Add CreateAccount api method

--- src/main/java/io/github/trevornelson/Dashboards.java
+++ src/main/java/io/github/trevornelson/Dashboards.java
@@ -4,16 +4,27 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.response.NotFoundException;
 import com.google.appengine.api.users.User;
+
 import javax.inject.Named;
 
 @Api(
     name = "dashboards",
     version = "v1",
-    scopes = {Constants.EMAIL_SCOPE},
+    scopes = {Constants.EMAIL_SCOPE, Constants.ANALYTICS_SCOPE},
     clientIds = {Constants.WEB_CLIENT_ID, Constants.ANDROID_CLIENT_ID, Constants.IOS_CLIENT_ID},
     audiences = {Constants.ANDROID_AUDIENCE}
 )
 public class Dashboards {
 
+	@ApiMethod(name = "dashboards.createAccount", httpMethod = "post")
+	public Account CreateAccount(final User user) {
+		String username = extractUsername(user.getEmail());	// add control flow to check if user already has account
+		String email = user.getEmail();
+		String userId = user.getUserId();
+		
+		return Account;
+	}
+	
+	
 
 }
