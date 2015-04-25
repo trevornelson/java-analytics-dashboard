eccc8369282069796131940dd0aa2c49ed041960
trevornelson <trevor.nelson1@gmail.com>
2015-04-25 15:29:16 -0400

    Add WEB_CLIENT_ID constant

--- src/main/java/io/github/trevornelson/Constants.java
+++ src/main/java/io/github/trevornelson/Constants.java
@@ -4,7 +4,7 @@ package io.github.trevornelson;
  * Contains the client IDs and scopes for allowed clients consuming the helloworld API.
  */
 public class Constants {
-  public static final String WEB_CLIENT_ID = "replace this with your web client ID";
+  public static final String WEB_CLIENT_ID = "99492469869-a8phf2icj5576p0a1or8v25djbqlb32k.apps.googleusercontent.com";
   public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
   public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
   public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
