75e937c2b876e6852ebe80ee0df5656f735d5516
trevornelson <trevor.nelson1@gmail.com>
2015-04-28 12:25:44 -0400

    Create app.js

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,7 @@
+/**
+ * @fileoverview
+ * Provides methods for connecting the app's frontend to the endpoints API.
+ * 
+ * @author trevor.nelson1@gmail.com (Trevor Nelson)
+ * Based off of the hello-endpoints-archectype.
+ */
