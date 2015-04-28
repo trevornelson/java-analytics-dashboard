b9a6757d1cf1a99d9a374e3378c118189d3a6452
trevornelson <trevor.nelson1@gmail.com>
2015-04-28 13:02:55 -0400

    Create initial JS app namespace

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -5,3 +5,21 @@
  * @author trevor.nelson1@gmail.com (Trevor Nelson)
  * Based off of the hello-endpoints-archectype.
  */
+
+/**
+ * Initialize the app namespace on pageload
+ */
+(function() {
+	window.App = {
+		Models: {},
+		Collections: {},
+		Views: {}
+	};
+	
+	/**
+	 * function for returning a template underscore object.
+	 */
+	window.template = function(id) {
+		return _.template( $('#' + id).html() );
+	};
+});
