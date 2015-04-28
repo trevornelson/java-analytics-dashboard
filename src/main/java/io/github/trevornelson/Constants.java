814025e2c674401fe75b385862848543d51f25e6
trevornelson <trevor.nelson1@gmail.com>
2015-04-28 09:09:36 -0400

    Added api explorer client id constant

--- src/main/java/io/github/trevornelson/Constants.java
+++ src/main/java/io/github/trevornelson/Constants.java
@@ -1,9 +1,12 @@
 package io.github.trevornelson;
 
+import com.google.api.server.spi.Constant;
+
 public class Constants {
   public static final String WEB_CLIENT_ID = "99492469869-a8phf2icj5576p0a1or8v25djbqlb32k.apps.googleusercontent.com";
   public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
   public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+  public static final String API_EXPLORER_CLIENT_ID = Constant.API_EXPLORER_CLIENT_ID;
   public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
 
   public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
