63d024b11577fba25296a53608ed11dec81e4f23
trevornelson <trevor.nelson1@gmail.com>
2015-05-02 19:35:17 -0400

    Add initial bar chart test implementation for dashboard widgets

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -179,7 +179,6 @@ App.Views.Widget = Backbone.View.extend({
 		
 		if (this.model) {
 			this.model.on('change', this.render, this);
-			console.log(this.model);
 		}
 		
 		// bind the view to the model's refreshData event
@@ -190,18 +189,44 @@ App.Views.Widget = Backbone.View.extend({
 	className: 'col-md-6',
 	template: template('widget-template'),
 	renderChart: function() {
-		console.log(this.model);
 		var resp = this.model.get('queryResponse');
-		console.log(resp.rows);
 		var data = [];
 		var legend = [];
-		console.log(resp.rows[0][1]);
 		for (i = 0; i < resp.rows.length; i++) {
 			data.push(resp.rows[i][1]);
 			legend.push(resp.rows[i][0]);
 		}
 		console.log(legend);
 		console.log(data);
+		
+		var width = this.$el.width(),
+			height = width / 2,
+			barHeight = height / data.length;
+		
+		
+		var x = d3.scale.linear()
+			.domain([0, d3.max(data)])
+			.range([0, width]);
+		
+		var chart = d3.select(this.el).select(".chart")
+			.attr("width", width)
+			.attr("height", height);
+		
+		var bar = chart.selectAll("g")
+			.data(data)
+			.enter().append("g")
+			.attr("transform", function(d, i) { return "translate(0, " + i * barHeight + ")"; });
+		
+		bar.append("rect")
+			.attr("width", x)
+			.attr("height", barHeight - 1);
+		
+		bar.append("text")
+			.attr("x", function(d) { return x(d) - 3; })
+			.attr("y", barHeight / 2)
+			.attr("dy", ".35em" )
+			.text(function(d) { return d; });
+		
 	},
 	render: function() {
 		this.$el.html(this.template(this.model.toJSON()));
